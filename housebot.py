9019fe38d5ea568313aba0d02e0ba4c1e14e6d69
MarcelCutts <marcel.cutts@gmail.com>
2015-04-06 00:17:28 +0100

    Moved the house attributes to filter for and the maximum age of listings into the config file. Clarified the purpose of the two core functions

--- housebot.py
+++ housebot.py
@@ -4,28 +4,14 @@ import google_maps
 
 import config
 
+# Find all properties that match our housing requirements
+matching_properties_zoopla = zoopla.get_properties(
+    max_listing_age=config.house_maximum_listing_age,
+    params=config.house_search_parameters
+)
 
-ADDRESSES_FILENAME = 'addresses.json'
-
-max_listing_age = 60*12  # in minutes
-
-# construct params
-params = {
-    'order_by': 'age',          # price, age
-    'ordering': '',             # descending (default), ascending
-    'listing_status': 'rent',
-    'area': 'London',
-    'minimum_price': '300',     # price per week
-    'maximum_price': '400',     # price per week
-    'minimum_beds': '3',
-    'maximum_beds': '4',
-    'furnished': '',            # furnished, unfurnished, part-furnished
-    'property_type': '',        # houses, flats
-    'page_number': '',          # the page number of results to request, default 1
-    'page_size': '100'          # the size of each page of results, default 10, maximum 100
-}
-
-
-
-matching_properties_zoopla = zoopla.get_properties(max_listing_age, params)
-matching_properties = google_maps.get_time_to_work(matching_properties_zoopla, config.user_addresses)
+# Find the group's commute time to all properties that passed the filtering process.
+matching_properties = google_maps.get_time_to_work(
+    selected_properties=matching_properties_zoopla,
+    addresses=config.house_member_commuting_destinations
+)
